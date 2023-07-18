d304c3c5e07112ca3714ee79432db577bf1fbf80
Amarambao <atajnik1@gmail.com>
2023-07-18 11:15:50 +0300

    делегаты

--- Homework №4.1/Program.cs
+++ Homework №4.1/Program.cs
@@ -8,10 +8,14 @@ namespace Homework__4._1
 {
     internal class Program
     {
+        public delegate string StringMethod1 (string str);
+
+
         static void Main(string[] args)
         {
-            string FromConsole = Console.ReadLine();
-
+            //string OriginalString = Console.ReadLine();
+            string OriginalString = "qqwweerrttyyuuiioopp   {{::''||<<>>??}}   aaAAssSSddDDffFFggGGhhHHjjJJkkKKllLL   [[;;..//'']]   ZZZXXXCCCVVVBBBNNNMMM";
+            
             string RemoveSymbols(string str)
             {
                 StringBuilder sb = new StringBuilder();
@@ -19,25 +23,22 @@ namespace Homework__4._1
                 {
                     char c = str[i];
                     int code = (int)c;
-                    if (code <= 0x5a && code >= 0x41)
-                    {
-                        sb.Append(c);
-                    }
-                    if (code <= 0x7a && code >= 0x61)
+                    if ((code <= 'Z' && code >= 'A') || (code <= 'z' && code >= 'a'))
                     {
                         sb.Append(c);
                     }
                 }
                 return sb.ToString();
             }
-
+            
             string AddSymbols(string str)
             {
-                StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder(str);
                 string str1 = Console.ReadLine();
-                return str += str1;
+                sb.Append(str1);
+                return sb.ToString();
             }
-
+            
             string RemoveDuplicateSymbols(string str)
             {
                 StringBuilder sb = new StringBuilder();
@@ -54,13 +55,102 @@ namespace Homework__4._1
                 }
                 return sb.ToString();
             }
-
-            void instuct(StringMethod1 parameter1, string str2)
+            
+            string StringToUpper(string str)
             {
-                parameter1.Invoke(str2);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < str.Length; i++)
+                {
+                    char c = str[i];
+                    int code = (int)c;
+                    if (code <= 'z' && code >= 'a')
+                    {
+                        sb.Append(char.ToUpper(c));
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+                return sb.ToString();
             }
 
-            public delegate string StringMethod1(string str);
+            string StringReverse(string str)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    char c = str[i];
+                    sb.Append(c);
+                }
+                return sb.ToString();
+            }
+
+            string ModifiedString1 = StringReverse(RemoveDuplicateSymbols(StringToUpper(RemoveSymbols(OriginalString))));
+            Console.WriteLine(ModifiedString1);
+
+            string Instruction1(StringMethod1 param1, StringMethod1 param2, StringMethod1 param3, StringMethod1 param4, StringMethod1 param5, string str)
+            {
+                string result1 = param1.Invoke(str);
+                string result2 = param2.Invoke(result1);
+                string result3 = param3.Invoke(result2);
+                string result4 = param4.Invoke(result3);
+                string result5 = param5.Invoke(result4);
+                return result5;
+            }
+
+            //string ModifiedString2 = Instruction1(AddSymbols, StringToUpper, RemoveDuplicateSymbols, RemoveSymbols, StringReverse, OriginalString);
+            //Console.WriteLine(ModifiedString2);
+
+            //Action<string> RemoveSpaces = delegate (string str)
+            //{
+            //    for (int i = 0; i < str.Length; i++)
+            //    {
+            //        if (str[i] == ' ')
+            //        {
+            //            str[i] = '.';
+            //        }
+            //    }
+            //};
+
+            Predicate<string> LongerThan21 = delegate (string str)
+                {
+                      return str.Length > 21;
+                };
+
+            Func <string, string> AddOrRemove = delegate (string str)
+            {
+                if (LongerThan21(str) == true)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        char c = str[i];
+                        sb.Append(c);
+                    }
+                    return sb.ToString();
+                } 
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        char c = str[i];
+                        sb.Append(c);
+                    }
+                    while (sb.Length < 21)
+                    {
+                        int j = str.Length - 1;
+                        sb.Append('A');
+                        j++;
+                    }
+                    return sb.ToString();
+                }
+            };
+            string StrLengthLess21 = "qaz   wsx";
+            Console.WriteLine(AddOrRemove(StrLengthLess21));
+            //RemoveSpaces(StrLengthLess21);
+            Console.WriteLine(StrLengthLess21);
         }
     }
 }
