73d9aec9ec4c9fd0c8a9d1f833ea6f1f41efae73
Amarambao <atajnik1@gmail.com>
2023-08-06 01:18:56 +0300

    Order

---
+++ Homework №6/Order.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__6
+{
+    internal class Order
+    {
+        const double convertionUSD = 3.09;
+        uint id;
+        string surname;
+        string name;
+        string fatherName;
+        string phoneNumber;
+        string customerAdress;
+        string product;
+        string productDescription;
+        double productPrice;
+        uint productCount;
+        public uint Id 
+        { 
+            get => id; 
+        }
+        public string Surname 
+        { 
+            get => surname; 
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    surname = value;
+            } 
+        }
+        public string Name 
+        { 
+            get => name;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    name = value;
+            }
+        }
+        public string FatherName 
+        { 
+            get => fatherName;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    fatherName = value;
+            }
+        }
+        public string PhoneNumber 
+        { 
+            get => phoneNumber;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    phoneNumber = value;
+            }
+        }
+        public string CustomerAdress 
+        { 
+            get => customerAdress;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    customerAdress = value;
+            }
+        }
+        public string Product 
+        { 
+            get =>  product;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    product = value;
+            }
+        }
+        public string ProductDescription 
+        { 
+            get => productDescription;
+            protected set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    productDescription = value;
+            }
+        }
+        public double ProductPrice 
+        { 
+            get => productPrice;
+            protected set
+            {
+                if (value > 0)
+                    productPrice = value;
+            }
+        }   
+        public uint ProductCount 
+        { 
+            get => productCount;
+            protected set
+            {
+                if (value != 0)
+                    productCount = value;
+            }
+        }
+        public double TotalPrice()
+        {
+            return (ProductCount * ProductPrice) / convertionUSD;
+        }
+        public Order (uint id, string Surname, string Name, string FatherName, string Product, string ProductDescription, double ProductPrice, uint ProductCount)
+        {
+            this.id = id;
+            this.Surname = Surname;
+            this.Name = Name;
+            this.FatherName = FatherName;
+            this.Product = Product;
+            this.ProductDescription = ProductDescription;
+            this.ProductPrice = ProductPrice;
+            this.ProductCount = ProductCount;
+        }
+        public Order (string PhoneNumber, string CustomerAdress) : this(1, "Ivanov", "Ivan", "Ivanovich", "bread", "borodinskyi", 2.55, 3)
+        {
+            this.PhoneNumber = PhoneNumber;
+            this.CustomerAdress = CustomerAdress;
+        }
+    }
+}
---
+++ Homework №6/Program.cs
@@ -0,0 +1,19 @@
+﻿using Homework__6;
+
+Order[] Orders = new Order[]
+{
+    new Order(2, "Borisov", "Boris", "Borisovich", "Milk", "Goat milk", 7.99, 9),
+    new Order("+375 (25) 123-45-67", "Suchareva"),
+    new Order(3, "Ivan", "Boris", "Dunnovich", "Icecream", "Vanilla", 5.25, 2)
+};
+
+Console.WriteLine(Orders[0].GetType());
+
+var Ascending =  Orders.OrderBy(order => order.TotalPrice());
+
+foreach (var order in Ascending)
+{
+    Console.WriteLine("{0:###.##}", order.TotalPrice());
+}
+
+Console.WriteLine(Orders[0] == Orders[1]);
